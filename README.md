1500ab1706c6219dad9c94aa0cf572bacdffb049
Kitty <linkittyp@gmail.com>
2025-10-04 13:45:38 +0200

    Dialogues work... this time

---
+++ README.md
@@ -0,0 +1,14 @@
+Haiii :333
+
+As you probably already noticed this lil silly project is made just for fun and nothing in here is professional. :3
+Every script sucks, every texture, sound, sprite, everything sucks :3
+
+Oh and if you want to open it somewhere else than on linux... well good luck.
+Sowwy I have a bit more serious problems with this thing than making it work on windows >~<
+Maybe someday ;3
+
+Oh yeah and if you are on linux and want to open this abbomination you'll have to go to this directory 'john-p/rust/target/debug/' and extract that one zip file there and leave that one .so file in this directory and it should work nicely :3
+Or alternatively you could get rid of that zip file and just go to rust directory and compile that library... if you have cargo of course.
+It'll take a while and a lot of space because somehow that godot-rust crate is MASSIVE >~<
+
+Anyways, Have fun xd
--- rust/src/npc.rs
+++ rust/src/npc.rs
@@ -1,4 +1,4 @@
-use godot::classes::{AnimatableBody2D, IAnimatableBody2D};
+use godot::classes::{AnimatableBody2D, Control, IAnimatableBody2D, Label};
 use godot::prelude::*;
 
 use crate::dialogues::Dialogues;
@@ -9,7 +9,12 @@ struct Npc {
     #[export]
     dialogues: Option<Gd<Dialogues>>,
 
-    interaction_counter: u32,
+    interacted: bool,
+    dialogue_counter: isize,
+    active_dialogue: Vec<GString>,
+
+    dialogue_box: Option<Gd<Control>>,
+    dialogue_label: Option<Gd<Label>>,
 
     base: Base<AnimatableBody2D>,
 }
@@ -18,16 +23,43 @@ struct Npc {
 impl Npc {
     #[func]
     fn interact(&mut self) {
+        let dialogue_box = self.dialogue_box.as_mut().unwrap();
+        let dialogue_label = self.dialogue_label.as_mut().unwrap();
+
         if let Some(dialogues) = &mut self.dialogues {
-            let f = if self.interaction_counter == 0 {
-                dialogues.call("get_dialogues_for", &["interact".to_string().to_variant()])
+            let ad = &mut self.active_dialogue;
+
+            let current = if ad.is_empty() {
+                *ad = if !self.interacted {
+                    dialogues.call("get_dialogues_for", &["interact".to_string().to_variant()])
+                } else {
+                    dialogues.call(
+                        "get_dialogues_for",
+                        &["interact_again".to_string().to_variant()],
+                    )
+                }
+                .to::<Vec<GString>>();
+
+                dialogue_box.set_visible(false);
+                self.dialogue_counter += 1;
+                return;
             } else {
-                dialogues.call("get_dialogues_for", &["interact_again".to_string().to_variant()])
+                dialogue_box.set_visible(true);
+                &ad[self.dialogue_counter as usize]
             };
 
-            godot_print!("dialogues: '{:?}'", f);
+            godot_print!("dialogues: '{:?}'", current);
+
+            dialogue_label.set_text(current);
+
+            self.dialogue_counter += 1;
+
+            if !ad.is_empty() && self.dialogue_counter >= ad.len() as isize {
+                self.interacted = true;
+                self.dialogue_counter = -1;
+                *ad = vec![];
+            }
         }
-        self.interaction_counter += 1;
     }
 }
 
@@ -36,8 +68,27 @@ impl IAnimatableBody2D for Npc {
     fn init(base: Base<AnimatableBody2D>) -> Self {
         Self {
             dialogues: None,
-            interaction_counter: 0,
+
+            interacted: false,
+            dialogue_counter: -1,
+            active_dialogue: vec![],
+
+            dialogue_box: None,
+            dialogue_label: None,
+
             base,
         }
+    }
+
+    fn enter_tree(&mut self) {
+        self.dialogue_box = Some(
+            self.base()
+                .find_child("DialogueBox")
+                .unwrap()
+                .cast::<Control>(),
+        );
+        self.dialogue_label = Some(self.base().find_child("Label").unwrap().cast::<Label>());
+
+        self.dialogue_box.as_mut().unwrap().set_visible(false);
     }
 }
